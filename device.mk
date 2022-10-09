26e65d34316d21ba49a8e0e8f04b44ff9e3dfdd9
rio004 <riyon336@gmail.com>
2022-10-09 21:37:23 +0000

    evergo: Build boot@1.1
    since 1.2 isnt in a11

--- device.mk
+++ device.mk
@@ -14,9 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-impl.recovery \
+    android.hardware.boot@1.1-service
 
 PRODUCT_PACKAGES += \
     update_engine \
