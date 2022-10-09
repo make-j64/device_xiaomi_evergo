0964c7cb6b0f2525353ef18bb2705bf1594c9806
wbs306 <wbs306@gmail.com>
2022-10-09 22:35:36 +0000

    evergo: Pull necessary bootjars and blobs

--- device.mk
+++ device.mk
@@ -49,6 +49,16 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
